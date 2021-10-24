3e51845f95f1797c908beeba4a8fe0d5cb4199c4
William Ganrot <williamganrot@hotmail.com>
2021-10-24 09:23:27 +0200

    remove comment

--- CQL.Library/Parser.fs
+++ CQL.Library/Parser.fs
@@ -147,111 +147,11 @@ module Parser =
     opp.AddOperator <| InfixOperator("*", spaces, 3, Associativity.Left, fun x y -> Binary (Multiply, x, y))
     opp.AddOperator <| InfixOperator("/", spaces, 4, Associativity.Left, fun x y -> Binary (Divide, x, y))
 
-    //let expressions = choice [numberExpression;
-    //                          stringExpression;
-    //                          columnExpression;
-    //                          boolExpression;
-    //                          binaryEqualit]
-    opp.TermParser <- (numberExpression <|> stringExpression <|> columnExpression <|> boolExpression .>> spaces) <|> between (spaces >>. pstring "(" >>. spaces) (spaces >>. pstring ")" >>. spaces) expr
-    //do expressionRef := choice [numberExpression;
-    //                                    stringExpression;
-    //                                    columnExpression;
-    //                                    boolExpression;
-    //                                    binaryEquality]
+    let expressionLittetrals = choice [numberExpression;
+                                       stringExpression;
+                                       columnExpression;
+                                       boolExpression;]
+    opp.TermParser <- (expressionLittetrals  .>> spaces) <|> between (spaces >>. pstring "(" >>. spaces) (spaces >>. pstring ")" >>. spaces) expr
+
 
     let parse = spaces >>. queryType
-//module Parser =
-//    open FParsec
-
-//    let alphabet = "abcdefghijklmnopqrstuvwxyz"
-//    let keywords =
-//        ["select"
-//         "from"
-//         "in"
-//         "as"
-//         "create"
-//         "left join"
-//         "inner join"]
-    
-//    let queryType, queryTypeRef = createParserForwardedToRef<Query, unit>()
-//    let fromType, fromTypeRef = createParserForwardedToRef<From, unit>()
-//    let joinType, joinTypeRef = createParserForwardedToRef<Join, unit>()
-//    let comparisonPartType, comparisonPartTypeRef = createParserForwardedToRef<ComparisonExpression, unit>()
-
-//    let alphastring : Parser<_,unit> = many1Chars (anyOf alphabet)
-//    let manyCharsBetween popen pclose pchar = popen >>? manyCharsTill pchar pclose
-//    let anyStringBetween popen pclose = manyCharsBetween popen pclose anyChar
-//    let anyStringBetweenStrings s1 s2 = anyStringBetween (pstring s1) (pstring s2)
-//    let singleQoutedString = anyStringBetweenStrings "'" "'"
-//    let doubleQoutedString = anyStringBetweenStrings "\"" "\""
-//    let betweenString s1 s2 p = between (pstring s1) (pstring s2) p
-
-//    let equal : Parser<_,unit> = stringReturn "=" Equals
-//    let notequal : Parser<_,unit> = stringReturn "<>" NotEquals
-
-//    let all = stringReturn "*" All
-//    let specificColumn = opt(attempt (manyCharsTill (anyOf alphabet) (pstring "."))) .>>. alphastring
-
-//    let intlitteral = pint32 .>> spaces |>> (fun i -> (float i)) |>> NumberExpression
-//    let floatlitteral = pfloat .>> spaces |>> NumberExpression
-
-//    let numberExpression = floatlitteral <|> intlitteral 
-//    let stringExpression = doubleQoutedString |>> StringExpression .>> spaces
-//    let columnExpression = betweenString "'" "'" specificColumn |>> ColumnExpression .>> spaces
-//    let boolExpression : Parser<_,unit> = ((stringReturn "true" (Bool 1)) <|> (stringReturn "false" (Bool 0)))
-//    let binaryEquality = betweenString "(" ")" (spaces >>. comparisonPartType .>> spaces .>>. (equal <|> notequal) .>>. (spaces >>. comparisonPartType)) |>> (fun ((x,y),z) -> Binary(y,x,z))
-
-//    let manyEqualityExpression = 
-//        let seperator = pstring "&&" .>> spaces 
-//        sepBy1 binaryEquality seperator
-
-//    let selectColumns = between (pstring "'") (pstring "'") (all <|> (specificColumn |>> Specifict))
-//    let where = pstring "where" >>. spaces >>. binaryEquality .>> spaces
-//    let manySelectParameter = 
-//        let seperator = pstring "," .>> spaces 
-//        sepBy1 selectColumns seperator
-
-//    let create = spaces >>. pstring "create" >>. spaces1 >>. singleQoutedString .>> spaces
-//    let select = spaces >>. pstring "select" >>. spaces1 >>. manySelectParameter .>> spaces1
-
-//    let innerjoin = pstring "inner join" >>. spaces1 >>. fromType .>> spaces .>>. manyEqualityExpression .>> spaces |>> InnerJoin
-//    let leftjoin = pstring "left join" >>. spaces1 >>. fromType .>> spaces .>>. manyEqualityExpression .>> spaces |>> LeftJoin
-
-//    let joins = opt(many1 joinType) |>> (fun joinlist ->
-//        match joinlist with
-//        | Some l -> l
-//        | None -> [])
-
-//    let wheres = opt (many1 (where)) |>> (fun wherelist ->
-//        match wherelist with
-//        | Some l -> l
-//        | None -> [])
-
-//    let alias = opt(pstring "as" >>. spaces1 >>. singleQoutedString)
-//    let from = pstring "from" .>> spaces1 >>. fromType .>> spaces 
-//    let tableName = singleQoutedString .>>. (spaces >>. alias)
-
-//    let selectQuery = select .>>. from .>>. joins .>>. wheres .>> spaces |>> fun(((x,y),z),a) -> SelectQuery(x,y,z,a)
-//    let selectQueryWitheof = selectQuery .>> eof 
-//    let subSelectQuery = ((between (pstring "(") (pstring ")") (selectQuery .>> spaces)) .>> spaces) .>>. alias
-
-//    let createQuery = create .>>. fromType .>> spaces |>> fun (filename,(from)) -> CreateQuery(filename,from)
-
-
-
-//    do queryTypeRef := choice [selectQueryWitheof |>> Select
-//                               createQuery |>> Create]
-
-//    do fromTypeRef := choice [tableName |>> TableName
-//                              subSelectQuery |>> SubQuery]
-
-//    do joinTypeRef := choice [innerjoin |>> Inner
-//                              leftjoin |>> Left]
-
-//    do comparisonPartTypeRef := choice [numberExpression;
-//                                        stringExpression;
-//                                        columnExpression;
-//                                        boolExpression;
-//                                        binaryEquality]
-
-//    let parse = spaces >>. queryType
