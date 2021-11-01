0425925886551a37c25934c0986b9fbc77812479
William Ganrot <williamganrot@hotmail.com>
2021-11-01 20:38:58 +0100

    can pass query as console argument

--- CQL.Console/Program.fs
+++ CQL.Console/Program.fs
@@ -1,5 +1,4 @@
 
-open System.IO
 open FParsec
 open CQL.Parser
 open CQL.Interpreter
@@ -7,12 +6,15 @@ open CQL.Interpreter
 [<EntryPoint>]
 let main argv =
 
-    //let q = "select '*' from 'person.txt' as 'p' inner join 'adress.txt' as 'a' 'a.ownerid' + 'p.userid' + 'p.userid' = 'p.userid' * 2 + 'a.ownerid'"
-    let q = "select '*' from 'person.txt' as 'p' left join 'adress.txt' as 'a' 'p.userid' = 'a.ownerid'" 
-    match (run Parser.parse q) with
-    | Success(result, _, _)  ->
-        printfn "%A" result
+    let args = argv |> Array.toList |> List.tail
 
-        Interpreter.eval result
+    let query =
+        args
+        |> List.head
+        |> fun s -> if (s.[0] = '"' && s.[s.Length-1] = '"') then s.[1..s.Length-1] else s
+
+    match (run Parser.parse query) with
+    | Success(result, _, _) -> Interpreter.eval result
     | Failure(errorMsg, _, _) -> printfn "Failure: %s" errorMsg
+
     0
